0096a9ddd784fa6c2e5a1b40eb9f383faa312fb6
AlexOros <alexanderbarber1986@gmail.com>
2023-08-15 14:43:49 +0300

    add fallback to getStaticPaths

--- src/pages/proiecte/[slug].tsx
+++ src/pages/proiecte/[slug].tsx
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
         },
         locale: attributes!.locale,
       })),
-      fallback: false,
+      fallback: true,
     };
   } catch (error) {}
 }
